c6cb315d29a001dfc7b5fcf48cd5b66bc53304c6
Jan Samaniego <100932229+Jansamaniego@users.noreply.github.com>
2023-12-30 01:15:28 +0800

    changed cookie option

--- src/config/cookie.js
+++ src/config/cookie.js
@@ -3,13 +3,13 @@ import config from './config';
 export const accessTokenCookieOptions = {
   maxAge: config.jwt.accessExpirationMinutes * 60 * 1000,
   httpOnly: false,
-  sameSite: 'Strict',
+  sameSite: 'Lax',
   secure: true
 };
 
 export const refreshTokenCookieOptions = {
   maxAge: config.jwt.refreshExpirationDays * 24 * 60 * 60 * 1000,
   httpOnly: false,
-  sameSite: 'Strict',
+  sameSite: 'Lax',
   secure: true
 };
