517165f8949dcd77cb335c8a5796ec2388e68bcc
Jan Samaniego <100932229+Jansamaniego@users.noreply.github.com>
2023-12-30 17:35:50 +0800

    removed sameSite option on cookies

--- src/config/cookie.js
+++ src/config/cookie.js
@@ -2,12 +2,10 @@ import config from './config';
 
 export const accessTokenCookieOptions = {
   maxAge: config.jwt.accessExpirationMinutes * 60 * 1000,
-  httpOnly: false,
-  sameSite: 'Lax',
+  httpOnly: false
 };
 
 export const refreshTokenCookieOptions = {
   maxAge: config.jwt.refreshExpirationDays * 24 * 60 * 60 * 1000,
-  httpOnly: false,
-  sameSite: 'Lax',
+  httpOnly: false
 };
