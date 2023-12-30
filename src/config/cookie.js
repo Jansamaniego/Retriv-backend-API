e42d6239ba164aa5833d3a74a7c2f2e1fd5eb0db
Jan Samaniego <100932229+Jansamaniego@users.noreply.github.com>
2023-12-30 17:21:18 +0800

    removed secure cookie option

--- src/config/cookie.js
+++ src/config/cookie.js
@@ -4,12 +4,10 @@ export const accessTokenCookieOptions = {
   maxAge: config.jwt.accessExpirationMinutes * 60 * 1000,
   httpOnly: false,
   sameSite: 'Lax',
-  secure: true
 };
 
 export const refreshTokenCookieOptions = {
   maxAge: config.jwt.refreshExpirationDays * 24 * 60 * 60 * 1000,
   httpOnly: false,
   sameSite: 'Lax',
-  secure: true
 };
